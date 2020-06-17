b85dfa653b5835f88f22672770c6ffde063da3ef
Joshua Kas <jjkas@uw.edu>
2020-06-17 10:53:58 -0700

    Changed run_test to do interpolation before comparing files.

---
+++ examples/rsqr.py
@@ -0,0 +1,14 @@
+import numpy as np
+fl1 = raw_input()
+fl2 = raw_input()
+c1 = input()-1
+c2 = input()-1
+x1,y1=np.loadtxt(fl1,usecols=(c1,c2),unpack=True)
+x2,y2=np.loadtxt(fl2,usecols=(c1,c2),unpack=True)
+xmin = max(x1[0],x2[0])
+xmax = min(x1[-1],x2[-1])
+x = np.linspace(xmin,xmax,100)
+y1_interp=np.interp(x,x1,y1)
+y2_interp=np.interp(x,x2,y2)
+rsqr = (y2_interp - y1_interp)**2/(y1_interp + y2_interp)
+print(np.average(rsqr))
